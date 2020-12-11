d1b72e9d44fb283954346f1ea757b3bbc781cdff
win10-sg <shigang@qhebusbar.net>
2020-12-11 17:08:00 +0800

    fix：修改布局

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <!-- <img src="./assets/logo.png" /> -->
-    <header-view></header-view>
+    <header-view class="header"></header-view>
     <!-- <router-view id="containner" /> -->
 
     <!-- 使用动态的 transition name -->
@@ -30,15 +30,23 @@ export default {
 
 <style>
 #app {
+  /* position: relative; */
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin: 0px;
-  padding: 0px;
+  margin: 0;
+  padding: 0;
 }
 #containner {
+  margin-top: 50px;
   min-height: 800px;
+}
+.header {
+  width: 100%;
+  position: fixed;
+  top: 0;
+  left: 0;
 }
 </style>
