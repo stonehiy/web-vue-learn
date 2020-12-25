d8547bbc43f68dc86eb138015e9ee5531a3b9103
win10-sg <shigang@qhebusbar.net>
2020-12-25 17:52:57 +0800

    add

---
+++ test/demo/webSocketServer.js
@@ -0,0 +1,15 @@
+
+var WebSocket = require('ws');
+
+var wss = new WebSocket.Server({ port: 10000 });
+ 
+wss.on('connection', function connection(ws) {
+    console.log('server: receive connection.');
+    
+    ws.on('message', function incoming(message) {
+        console.log('server: received: %s', message);
+    });
+ 
+ws.send('world');
+
+});
